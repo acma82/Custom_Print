ddd20df24b3113a62ae3caa2030650cacc557eef
acma82 <acma.mex@gmail.com>
2025-09-21 22:34:31 -0600

    checking to don't put the code

--- 0_00_screen_funs.py
+++ 0_00_screen_funs.py
@@ -15,7 +15,7 @@ from custom_print import dimensions
 print("fp.clear() -> clear function")
 print("Clear the terminal and restore to home the cursor")
 print("It uses the system cmd to clear all the screen")
-time.sleep(4)
+time.sleep(3)
 clear()
 
 
@@ -23,7 +23,7 @@ clear()
 cols, rows = dimensions()
 print("fp.dimensions() -> It returns the dimensions of the screen")
 print("cols: ", cols, "  rows: ", rows)
-time.sleep(4)
+time.sleep(3)
 
 
 for n in range(5):
@@ -32,7 +32,7 @@ for n in range(5):
 print("erase() function cleans the terminal and the cursor remain in the same position")
 print("erase uses the ansi system")
 erase()
-time.sleep(4)
+time.sleep(3)
 
 
 print("keep going... with clean")
